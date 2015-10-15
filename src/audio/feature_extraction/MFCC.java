e47caa5dc28bc11da1c5f7b5cee2223c54706150
fathurrohmane <f4thurz@gmail.com>
2015-10-15 15:11:17 +0700

    MFCC - 100% ->Done -Filterbank -DCT ->Remaining -test mfcc

--- src/audio/feature_extraction/MFCC.java
+++ src/audio/feature_extraction/MFCC.java
@@ -1,6 +1,8 @@
 package audio.feature_extraction;
 
 import audio.ComplexVector;
+import data.Complex;
+import data.FFT;
 
 /**
  * Created by Fathurrohman on 5/20/2015.
@@ -25,13 +27,20 @@ public class MFCC {
     double[] audioData;
     double[][] framedAudioData;
     //Audio data in complex format for FFT
-    ComplexVector[] complexVectors;
+//    ComplexVector[] complexVectors;
+    Complex[][] complexes;
 
     //mag spectrume
     double[][] magSpectrum;
 
     //filterBank
     int[] cbin;
+
+    //
+    double[][] tempMelFilter;
+
+    //Ceptra (MFCC Coeeficient)
+    double[][] ceptra;
 
     // preEmphasis coefficient
     float preEmphasisCoefficient = 0.95f;
@@ -52,12 +61,15 @@ public class MFCC {
         samplePerFrameStep = 1024; // 1024 / 96000 = 0.010 | 10ms (overlap frame size)
 
 
-        noOfFrames = (int) (2 * Math.ceil(sampleLength / samplePerFrame)); // overlap 50% (X2)
+        noOfFrames = (int) (Math.ceil(sampleLength / samplePerFrame)) + 1; // overlap 50% (X2)
 
         preEmphasis();
         framing();
         windowing();
         fft();
+        makeFilterBank();
+        melFilter();
+        dct();
 
     }
 
@@ -111,34 +123,160 @@ public class MFCC {
 
     private void fft() {
 
-        complexVectors = new ComplexVector[framedAudioData.length];
+        //complexVectors = new ComplexVector[framedAudioData.length];
+        complexes = new Complex[framedAudioData.length][samplePerFrame];
 
         //Compute fft per frames
         //loop per frame
-        for (int i = 0; i < framedAudioData.length; i++) {
-            complexVectors[i] = new ComplexVector(framedAudioData[i]);
+        //for (int i = 0; i < framedAudioData.length; i++) {
+        //complexVectors[i] = new ComplexVector(framedAudioData[i]);
+        //}
+
+        Complex[][] complexAfterFFT = new Complex[framedAudioData.length][samplePerFrame];
+
+        for (int i = 0; i < noOfFrames; i++) {
+            for (int j = 0; j < samplePerFrame; j++) {
+            complexes[i][j] = new Complex(framedAudioData[i][j],0.0);
+            }
+            complexAfterFFT[i] = FFT.fft(complexes[i]);
         }
+
         System.out.println("Done FFT");
 
         //calculate mag spectrume
-        magSpectrum = new double[noOfFrames][framedAudioData.length];
+        magSpectrum = new double[noOfFrames][framedAudioData[0].length];
         for (int i = 0; i < noOfFrames; i++) {
-            for (int j = 0; j < framedAudioData.length; j++) {
-                magSpectrum[i][j] = Math.sqrt(complexVectors[i].real[j] * complexVectors[i].real[j] + complexVectors[i].imaginary[j] * complexVectors[i].imaginary[j]);
+            for (int j = 0; j < framedAudioData[0].length; j++) {
+          //      magSpectrum[i][j] = Math.sqrt(complexVectors[i].real[j] * complexVectors[i].real[j] + complexVectors[i].imaginary[j] * complexVectors[i].imaginary[j]);
+                magSpectrum[i][j] = Math.sqrt(Math.pow(complexAfterFFT[i][j].re(),2) + Math.pow(complexAfterFFT[i][j].im(),2));
             }
         }
+
+        System.out.println("Done Mag");
     }
 
     private void makeFilterBank() {
+        //initiate array
         cbin = new int[numMelFilter + 2];
+        double[] cbinTemp = new double[numMelFilter + 2];
 
+        //create range frequency based on mel-scale
         double lowMel = hzToMel(lowerFilterFrequency);
-        double highMel = hzToMel(lowerFilterFrequency);
+        double highMel = hzToMel(higerFilterFrequency);
+
+        //initiate 1st and last filter bank
+        cbinTemp[0] = lowMel;
+        cbinTemp[cbin.length - 1] = highMel;
+
+        //create filter bank in mel-scale
+        for (int i = 1; i <numMelFilter + 1; i++) {
+            cbinTemp[i] = lowMel + ((highMel - lowMel) / (numMelFilter + 2)) * i;
+        }
+
+        //convert back to frequency
+        for (int i = 0; i <= numMelFilter + 1; i++) {
+            cbin[i] = (int) Math.floor(samplePerFrame*melToHz(cbinTemp[i]) / sampleRate);
+            //cbinTemp[i] = melToHz(cbinTemp[i]);
+        }
+
+//        double[][] filterBank = new double[numMelFilter][100];
+//        //make filterbank
+//        for (int i = 0; i <= numMelFilter + 1; i++) {
+//            for (int j = cbin[i]; j < cbin[i + 1]; j++) {
+//                if((cbin[i + 1] - cbin[j]) != 0) {
+//                    filterBank[i][j] = (j - cbin[i]) / (cbin[i + 1] - cbin[j]);
+//                }
+//            }
+//            for (int j = cbin[i + 1]; j < cbin[i + 2]; j++) {
+//                if((cbin[i + 2] - cbin[i + 1]) != 0) {
+//                    filterBank[i][j] = (cbin[i + 2] - j) / (cbin[i + 2] - cbin[i + 1]);
+//                }
+//            }
+//        }
+        System.out.println("LOL");
 
     }
 
+    private void melFilter() {
+        tempMelFilter = new double[noOfFrames][numMelFilter];
+        //double[] temp = new double[numMelFilter + 2];
+        //Loop through frames
+        for (int i = 0; i < noOfFrames; i++) {
+            for (int j = 1; j <= numMelFilter - 2; j++) {
+// VERSION CODE 3
+                int startFreqId = cbin[j - 1];
+                int centerFreqId = cbin[j];
+                int endFreqId = cbin[j + 1];
+
+                for (int freq = startFreqId; freq < centerFreqId; freq++) {
+                    int magnitudeScale = centerFreqId - startFreqId;
+                    tempMelFilter[i][j] += magSpectrum[i][freq]*(freq - startFreqId) / magnitudeScale;
+                }
+
+                for (int freq = centerFreqId; freq < endFreqId; freq++) {
+                    int magnitudeScale = centerFreqId - endFreqId;
+                    tempMelFilter[i][j] += magSpectrum[i][freq]*(freq - endFreqId) / magnitudeScale;
+                }
+
+  /*              double num1 = 0;
+                double num2 = 0;
+
+                double den = (cbin[j] - cbin[j - 1] + 1);
+
+                for (int k = cbin[j - 1]; k <= cbin[j] ; k++) {
+                    num1 += magSpectrum[i][k] * (k - cbin[j - 1] + 1);
+                }
+                num1 /= den;
+
+                den = (cbin[j + 1] - cbin[j] + 1);
+
+                for (int k = cbin[j] + 1; k <= cbin[j + 1]; k++) {
+                    num2 += magSpectrum[i][k] * (1 - ((k - cbin[j]) / den));
+                }
+  */
+
+//                for (int k = cbin[j - 1]; k <= cbin[j]; k++) {
+//                    num1 += ((k - cbin[j - 1] + 1)/
+//                            (cbin[j] - cbin[j - 1] + 1))
+//                            * magSpectrum[i][k];
+//                }
+//
+//                for (int k = cbin[j - 1]; k <= cbin[j]; k++) {
+//                    num2 += (1 - ((k - cbin[j] ) /
+//                            (cbin[j + 1] - cbin[j] + 1)))
+//                            * magSpectrum[i][k];
+//                }
+
+               //temp[j] = num1 + num2;
+
+
+            }
+
+//            for (int j = 0; j < numMelFilter; j++) {
+//                tempMelFilter[i][j] = temp[j + 1];
+//            }
+//            System.out.println("LOL");
+        }
+    }
+
+    private void dct()
+    {
+        int numCeptra = 12;
+        ceptra = new double[noOfFrames][numCeptra];
+
+        for (int i = 0; i < noOfFrames; i++) {
+            for (int j = 1; j <= numCeptra; j++) {
+                for (int k = 1; k <= numMelFilter ; k++) {
+                    ceptra[i][j - 1] += tempMelFilter[i][k - 1] * Math.cos(Math.PI * (j - 1) / numMelFilter * (k - 0.5));
+                }
+            }
+        }
+        System.out.println("DCT Done");
+        System.out.println("MFCC Done");
+    }
+
     private double hzToMel(double hz) {
-        return 2595*Math.log10(1+hz/700);
+        return 2595 * Math.log10(1 + hz / 700);
     }
 
     private double melToHz(double x) {
