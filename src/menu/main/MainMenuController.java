550dc531c1595883a6040b6cd09ed172c01ce4c3
Fathurrohman Elkusnandi <f4thurz@gmail.com>
2018-02-04 13:25:27 +0700

    Add widget injection to MainMenuController

--- src/menu/main/MainMenuController.java
+++ src/menu/main/MainMenuController.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import tools.IMainView;
@@ -13,10 +17,24 @@ import java.awt.*;
 
 public class MainMenuController extends Application implements IMainView {
 
-    private Stage stage;
-
+    public CheckBox checkBoxReduceDimension;
+    public TextField textFieldDimensionReductionNumber;
+    public Button buttonReset;
+    public Button buttonStopProcess;
+    public Button buttonTesting;
+    public Button buttonTraining;
+    public Label textLabelDatabaseLocation;
+    public TextArea textAreaTrainedWordList;
+    public CheckBox checkBoxShowMFCCLog;
+    public CheckBox checkBoxShowPCALog;
+    public CheckBox checkBoxShowVQLog;
+    public CheckBox checkBoxShowHMMLog;
+    public CheckBox checkBoxShowCalculationTime;
+    public Label textLabelRecognitionRate;
     public TextField textFieldSoundDirectory;
     public TextField textFieldDatabaseDirectory;
+
+    private Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
